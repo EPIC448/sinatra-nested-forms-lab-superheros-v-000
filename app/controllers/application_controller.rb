1468bb97efdab3ce99ad8cba55de25ed3b0c6c1c
Sam. O <samrey2018@gmail.com>
2018-04-13 15:47:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class App < Sinatra::Base
 
  post '/teams' do
   @team = Team.new(params[:team])
-  binding.pry
 
    params[:team].each do |details|
+     binding.pry
 
     Team.new(details)
     end
