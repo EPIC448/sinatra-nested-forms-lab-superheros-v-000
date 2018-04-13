4fa03d8f6d6e7a8d46d5c9cca4b62e1ad027a200
Sam. O <samrey2018@gmail.com>
2018-04-13 16:38:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,8 @@ class App < Sinatra::Base
   @hero = Hero.new(info)
 end
     @heros= Hero.all
-    #
+# we have the super hero and there features here..
+# we go into teams.rb and iterate over it on line 9
     erb :teams
  end
 
